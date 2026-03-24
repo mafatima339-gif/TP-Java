080ec13d94499712556d62dea3303eff15392f8e
Fatima EL MANSORI <mafatima339@gmail.com>
2026-03-24 15:16:36 +0100

    Update Ex7.java

--- src/partie2/Ex7.java
+++ src/partie2/Ex7.java
@@ -4,27 +4,29 @@ import java.util.Scanner;
 
 public class Ex7 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Entrez la taille du tableau :");
-		int n = sc.nextInt();
-		int[] tab = new int[n];
-		for ( int i = 0; i<n ; i++) {
-			System.out.print("Entrez l'element " + i + ":");
-			tab[i] = sc.nextInt();
-			int max = tab[0];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	        for (int i1 = 1; i1 < n; i1++) {
-	            if (tab[i1] > max) {
-	                max = tab[i1];
-	            }
-	        }
+        System.out.print("Entrez la taille du tableau : ");
+        int n = sc.nextInt();
 
-	        System.out.println("Le plus grand element est : " + max);
-			
-		}
-		sc.close();
+        int[] tab = new int[n];
 
-	}
+        for (int i = 0; i < n; i++) {
+            System.out.print("Entrez l'element " + i + " : ");
+            tab[i] = sc.nextInt();
+        }
 
+        int max = tab[0];
+
+        for (int i = 1; i < n; i++) {
+            if (tab[i] > max) {
+                max = tab[i];
+            }
+        }
+        
+        System.out.println("Le plus grand element est : " + max);
+
+        sc.close();
+    }
 }
