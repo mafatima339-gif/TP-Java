d63021427a27d4ced44c4ecfce4bd0d4f41416b7
Fatima EL MANSORI <mafatima339@gmail.com>
2026-03-24 20:13:53 +0100

    Update Ex14.java

--- src/partie4/Ex14.java
+++ src/partie4/Ex14.java
@@ -2,7 +2,7 @@ package partie4;
 import java.util.Scanner;
 
 
-public class Ex13 {
+public class Ex14 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
